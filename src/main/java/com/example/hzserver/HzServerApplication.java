aa85588b4e99f9e289bf5405d8e7e837ba3bf58d
Alex <alex.gosu@gmail.com>
2023-08-20 23:30:10 +0600

    minor changes

--- src/main/java/com/example/hzserver/HzServerApplication.java
+++ src/main/java/com/example/hzserver/HzServerApplication.java
@@ -1,21 +1,12 @@
 package com.example.hzserver;
 
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HzServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HzServerApplication.class, args);
-    }
-
-    @Bean
-    public HazelcastInstance hazelcastInstance() {
-        return Hazelcast.newHazelcastInstance();
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(HzServerApplication.class, args);
+  }
 }
